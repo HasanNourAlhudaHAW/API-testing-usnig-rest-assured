3ecb24e8b3aa716a4b1f3e523ccee0b8ef10362b
HusseinNourAlhuda <husseinnour00@gmail.com>
2022-12-28 13:34:00 +0100

    adjust Methode testConflict

--- src/test/java/testPackage/TestKlasse.java
+++ src/test/java/testPackage/TestKlasse.java
@@ -2,10 +2,7 @@ package testPackage;
 
 public class TestKlasse {
 
-    void testConflict(){
-        int i = 3;
-        int y= 4;
-        int result = i+y;
-        System.out.println(result);
+    int testConflict(int summand1, int summand2, int subtrahent){
+        return summand1+summand2-subtrahent;
     }
 }
